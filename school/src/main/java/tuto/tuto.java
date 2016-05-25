5626f8dd508deb587329acec4dc560de89aa79fc
schangmokai <doumtsopmelifranck@yahoo.fr>
2016-05-25 06:04:24 +0100

    tuto tuto

---
+++ school/src/main/java/tuto/tuto.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuto;
+
+/**
+ *
+ * @author doumtsop
+ */
+public class tuto {
+//    Bonjour et Bienvenu sur ce tutoriel app Java.
+//    
+//    pour commencer créer unn projet maven comme suit file/newProjet/mave/javaApplication
+//    
+//    aller dans  le dossier projetfile du projet school puis dans pom.xml copier le contenu
+//            
+//    a partir de <properties> jusqua la derniere balise </project>.
+//    
+//    puis coller dans le pom.xml de votre projet a partir de <properties> comme precedement.
+//           
+//    apres ceci, créer donc le package data ou vous aller créer toutes les classes de votre diagramme de classe 
+//    
+//    ensuite passer au deuxieme package Dao interface exactement comme dans la projet school
+//    
+//    puis le troisieme package Dao implements.
+//            
+//    une foi ceci terminé, vous devez créer une base de donnée pour moi "school" et tappans mysql -u root -p
+//    
+//    ensuite >> create database school;
+//            
+//    puis configurons le fichier de persistence.
+//    
+//    clic droit sur le dossier du projet puis new ensuite persistenceUnit.
+//    
+//    ensuite le nom de l'unité de persistence ' exemple schoolPU ensuite vous choisissez hibernate puis create new database
+//            
+//    vous remplacer mysql par le nom de votre bd chez moi c school et vous metter le mot de passe en fin test de connection.
+//    
+//    connection succefful. COOLLLLLL!!!!!
+//    
+//    nous pouvons tester notre DAO
+//            
+//    aller dans testDao de school vous le copier et vous le coller dans votre projet vous changer l uniter de persistence qui e
+//         
+//    indiquer a la ligne  25 et vous mettez les element de la classe a tester pour moi c Ensignant qui est en teste.
+//    
+//    NB: changer les deux  premieres ligne du fichier de persistence que vous avez créee par 
+//            
+//    <?xml version="1.0" encoding="UTF-8"?>
+//    <persistence version="2.0" xmlns="http://java.sun.com/xml/ns/persistence" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/persistence http://java.sun.com/xml/ns/persistence/persistence_2_0.xsd">       
+//    
+//    FIN et la DAO doit marcher.
+    
+    
+    
+    
+    
+    
+            
+    
+            
+            
+}
